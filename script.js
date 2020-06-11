4b557ce4bef7dd93b11cb413f54701e6cc8be215
Jinhoo Bong <jinhoobong@gmail.com>
2020-06-10 17:19:07 -0700

    Working version of the game in initial stages!

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const gridItemArr = document.getElementsByClassName("grid-item");
 let gameTitle = document.getElementById("gameTitle");
 let levelTitle = document.getElementById("score");
-let directions = document.getElementById("directions");
+let gameover_message = document.getElementById("gameOverMessage");
+let playagain = document.getElementById("playagain");
 
 let offset = 75;
 let score = 0;
@@ -9,6 +10,10 @@ let level = 1;
 let offSquareIndex = 0;
 let wholeGameReset = false;
 // let play = true;
+
+for(let i = 0; i < gridItemArr.length; i++) {
+    gridItemArr[i].id = i; 
+}
 
 /* ------------------------------------------------------------------------------------------------------------------------ */
 
@@ -64,109 +69,96 @@ let commonColor = function() {
 
 // checkAnswer() : function that checks if the provided square is the one that the user chooses.
 const checkAnswer = function() {
-    // adding event listener to each item 
-    for(let i = 0; i < gridItemArr.length; i++) {
-        gridItemArr[i].addEventListener("click", function() {
-            // if user chooses correctly:
-            //      - add 100 to score
-            //      - level + 1
-            //      - update the gameTitle html
-            //      - reset for new squares 
-            if(i === offSquareIndex) {
-                score+=100;
-                level++;
-                gameTitle.innerHTML = "Level " + level;
-                levelTitle.innerHTML = score;
-                console.log("score: " + score);
-                console.log("Level: " + level);
-                reset();
-            } else {
-                console.log("Incorrect!");
-                // play = false;
-                // implement lose function
-                gameOver();
-            }
-        })
+
+    // if what the user has clicked is correct: 
+    //      - add 100 to score && display score
+    //      - increment level && display level
+    if(this.id == offSquareIndex) {
+        score+=100;
+        level++;
+
+        console.log("reached");
+        gameTitle.innerHTML = "Level " + level;
+        levelTitle.style.display = "block";
+        levelTitle.innerHTML = score;
+        reset();
+    } else {
+        gameOver();
     }
 }
 
-// resetColors() : function that generates new set of colors and populates the squares properly
+// reset() : function that generates new set of colors and populates the squares properly
 const reset = function() {
+    setupColors();
+}
 
-    // if the whole game is reset, we want the text to reflect the start of the game.
-    if(wholeGameReset) {
-        gameTitle.innerHTML = "Color Game";
-        levelTitle.innerHTML = "";
-        directions.style.display = "none";
-    }
 
-    // creating new sets of colors
-    let newColors = commonColor();
+// setupColors() : reusable function to set the color combinations and set up the grid
+const setupColors = function() {
 
-    // generating the new position of the square that will be one color off
+    // generating the two new RGB combinations
+    let colors = commonColor();
+
+    // generating the index of the off square
     offSquareIndex = Math.floor((Math.random() * gridItemArr.length));
-    console.log("reset 1: " + offSquareIndex);
-
-    // setting the squares up
+    
+    // setting up the physical grid and adding a click event listener to each square 
     for(let i = 0; i < gridItemArr.length; i++) {
         gridItemArr[i].style.display = "block";
+        gridItemArr[i].addEventListener("click", checkAnswer);
         if(i === offSquareIndex) {
-            gridItemArr[i].style.backgroundColor = newColors[1];
+            gridItemArr[i].style.backgroundColor = colors[1];
         } else {
-            gridItemArr[i].style.backgroundColor = newColors[0];
+            gridItemArr[i].style.backgroundColor = colors[0];
         }
     }
-    console.log("reset 2: " + offSquareIndex);
-    checkAnswer(offSquareIndex);
 }
+
 
 // Init() : function that sets up the initial stages of the game. 
 // We set the title of the Game, generate the colors, generate which square will be off, and then set up the squares accurately
 let init = function() {
 
     gameTitle.innerHTML = "Color Game";
-    levelTitle.style.display = "block";
     levelTitle.innerHTML = "";
-    directions.style.display = "none";
+    gameover_message.style.display = "none";
+    playagain.style.display = "none";
+
     // populating the two colors;
     let colors = commonColor();
     // console.log(colors);
     // generating the position of the square that will be one color off
     offSquareIndex = Math.floor((Math.random() * gridItemArr.length));
-    console.log("init 1: " + offSquareIndex);
+    // console.log("init 1: " + offSquareIndex);
 
     // setting the squares up 
-    for(let i = 0; i < gridItemArr.length; i++) {
-        gridItemArr[i].style.display = "block";
-        if(i === offSquareIndex) {
-            // console.log(arrOfColors[1]);
-            gridItemArr[i].style.backgroundColor = colors[1];
-        } else {
-            gridItemArr[i].style.backgroundColor = colors[0];
-        }
-    }
-    console.log("init 2: " + offSquareIndex);
-    checkAnswer(offSquareIndex);
+    setupColors();
+    console.log("init: " + offSquareIndex);
 }
 
 // gameOver() : when user guesses incorrectly, we alert the user and restart the game within a second
 const gameOver = function() {
+    console.log("gameOver entered");
+    // set wholeGameRest to true so that reset() method will update the titles 
     wholeGameReset = true;
+
+    // reset level && score
     level = 1;
     score = 0;
+
+    // display temporary game over message 
     gameTitle.innerHTML = "GAME OVER";
     levelTitle.style.display = "none";
     for(let i = 0; i < gridItemArr.length; i++) {
         gridItemArr[i].style.display = "none";
     }
-    directions.style.display = "block";
-    directions.innerHTML = "Game restarting... ";
+    gameover_message.style.display = "block";
+    gameover_message.innerHTML = "Game restarting... ";
+
     setTimeout(function() {
-       reset();
+       init();
     }, 1000);
 }
-
-
 
 // start the game
 init();
